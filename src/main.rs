20125297e643f70977a95e92f282b2ae02d5dd80
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-03-28 20:55:32 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("===== GUESS THE NUMBER =====");
+    let secret_number = rand::thread_rng().gen_range(1, 11);
+
+    loop {
+        println!("\nPlease input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a digit!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("YOU WIN!");
+                break;
+            }
+        }
+    }
+}
